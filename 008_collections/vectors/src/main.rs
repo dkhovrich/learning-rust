4d2e8d113dfae747ef165fd1f0b20f0b304729ea
Dmitriy Khovrich <dkhovrich@readdle.com>
2021-06-05 21:24:59 +0300

    vectors practive

--- 008_collections/vectors/src/main.rs
+++ 008_collections/vectors/src/main.rs
@@ -1,42 +1,22 @@
+use std::collections::HashMap;
+
 fn main() {
-    let v: Vec<u32> = Vec::new();
-    let mut v = vec![1, 2, 3];
+    let items = vec![4, 3, 5, 1, 2, 19, 9, 9];
+    println!("Items: {:?}", items);
 
-    v.push(4);
+    let mut item_count = HashMap::new();
+    let mut total = 0;
 
-    let a = &v[2];
-
-    println!("vector: {:?}, a: {}", v, a);
-
-    if let Some(b) = v.get(2) {
-        println!("b: {}", b);
+    for i in items.iter() {
+        total += i;
+        let count = item_count.entry(i).or_insert(0);
+        *count += 1;
     }
 
-    match v.get(2) {
-        Some(val) => println!("Value is: {}", { val }),
-        _ => {}
+    let values: Vec<i32> = item_count.values().copied().collect();
+
+    match values.iter().max() {
+        Some(v) => println!("Most frequent value: {}", v),
+        None => println!("Empty"),
     }
-
-    for i in &mut v {
-        *i += 50;
-    }
-
-    for i in &v {
-        println!("i:{}", i);
-    }
-
-    #[derive(Debug)]
-    enum SpreadsheetCell {
-        Int(i32),
-        Float(f64),
-        Text(String),
-    }
-
-    let row = vec![
-        SpreadsheetCell::Int(3),
-        SpreadsheetCell::Text(String::from("blue")),
-        SpreadsheetCell::Float(10.12),
-    ];
-
-    println!("Row:{:?}", row);
 }
